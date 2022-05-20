abe2940ee43f1847f8215969b8a255db704384ea
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-20 21:19:53 +0300

    Add classwork for lesson 3

---
+++ lesson_03/classwork.py
@@ -0,0 +1,39 @@
+# 1
+print(17 / 2 * 3 + 2)
+print(2 + 17 / 2 * 3)
+print(19 % 4 + 15 / 2 * 3)
+print((15 + 6) - 10 * 4)
+print(17 / 2 % 2 * 3)
+
+# 2
+print(17 / 2 * (3 + 2))
+print((2 + 17) / 2 * 3)
+print(19 % (4 + 15) / 2 * 3)
+print(((15 + 6) - 10) * 4)
+print(17 / 2 % (2 * 3))
+
+# 3
+denis = 26
+anton = 24
+vlad = 28
+sum_age = denis + anton + vlad
+print(sum_age)
+
+# 4
+mid_age = (denis + anton + vlad) / 3
+print(mid_age)
+
+# 5
+my_list = [1, 10, 2, 2, 5.0, "python", "python3", "python3"]
+my_set = set(my_list)
+print(len(my_list) - len(my_set))
+
+# 6
+my_slice = my_list[2:5][::-1]
+print(my_slice)
+
+# 7
+lenght =  5
+square = [lenght / 4, lenght ** 2, (lenght ** 2 * 2) ** 0.5]
+print(square)
+

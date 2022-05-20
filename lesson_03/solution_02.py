b45107bf453c750374d4e9d3766ed09720a70033
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-20 21:50:09 +0300

    Fix solution_2.py

--- lesson_03/solution_02.py
+++ lesson_03/solution_02.py
@@ -5,3 +5,15 @@ for i in range(5):
 
 print(money)
 
+
+# alternative
+
+money2 = 2130
+money2 += money2 * 0.1 + 120
+money2 += money2 * 0.1 + 120
+money2 += money2 * 0.1 + 120
+money2 += money2 * 0.1 + 120
+money2 += money2 * 0.1 + 120
+
+print(money2)
+

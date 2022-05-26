2d77635d944a1bd22c4e518320a425b727c5b126
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-26 23:55:37 +0300

    Add solution_02.py

---
+++ lesson_04/solution_02.py
@@ -0,0 +1,17 @@
+# Ввести с клавиатуры строку, проверить является ли строка палиндромом и вывести результат (yes/no) на экран. Палиндром - это слово или фраза, которые одинаково читаются слева направо и справа налево
+
+my_string = input("Введите строку: ")
+
+index = 0
+reverse_index = -1
+message = "Yes"
+
+while index != len(my_string):
+    if my_string[index] != my_string[reverse_index]:
+        message = "No"
+        break
+    else:
+        index += 1
+        reverse_index -= 1
+
+print(message)

681d4be0724ca8cf9ed39aaa30f51f4b4134ed6d
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-26 23:44:26 +0300

    Add classwork_07.py

---
+++ lesson_04/classwork_07.py
@@ -0,0 +1,25 @@
+"""
+Пользователь вводит с клавиатуры числа до тех пор, пока не введет любой строчный символ, из этих чисел составляется
+первый список. Далее таким же образом вводятся второй и третий списки. Вывести на экран список, элементы которого есть
+в первых двух списках, но отсутствуют в третьем.
+"""
+
+list1 = []
+list2 = []
+list3 = []
+
+for i in (list1, list2, list3):
+    while True:
+        num = input("Введите число: ")
+        if num.isdigit():
+            i.append(int(num))
+        else:
+            break
+
+res = []
+
+for i in (*list1, *list2):
+    if i in list1 and i in list2 and i not in list3 and i not in res:
+        res.append(i)
+
+print(res)

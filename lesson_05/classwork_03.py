b0658d94ef9ff864f86cb1c0d7a3975ddf913e90
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-27 21:26:32 +0300

    Fix classwork_03.py

--- lesson_05/classwork_03.py
+++ lesson_05/classwork_03.py
@@ -12,11 +12,19 @@ def my_func(func_type:str, *args):
 
 
 def my_max(*args):
-    return max(args)
+    max_item = args[0]
+    for item in args[1:]:
+        if item > max_item:
+            max_item = item
+    return max_item
 
 
 def my_min(*args):
-    return min(args)
+    min_item = args[0]
+    for item in args[1:]:
+        if item < min_item:
+            min_item = item
+    return min_item
 
 
-print(my_func('max', 4, 3, 5, 6, 6, 9))
+print(my_func('min', 4, 3, 5, 6, 6, 9))

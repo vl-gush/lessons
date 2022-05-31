8994bccf791e009352d25135bdfce3a72321952e
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-05-31 22:16:28 +0300

    Add solution_03.py

---
+++ lesson_05/solution_03.py
@@ -0,0 +1,14 @@
+"""
+Написать функцию, которая используя модуль requests скачивает файл сохраняет его на файловой системе, функция имеет два
+параметра: ссылка на файл и имя на файловой системе. В качестве примера ссылки на файл можно использовать лицензию из
+ГитХаба из вашего репозитория: https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE
+"""
+import requests
+
+def download_file(file_url, file_path):
+    responce = requests.get(file_url).text
+    with open(file_path, "w") as file:
+        file.write(responce)
+
+
+download_file("https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE", "example")

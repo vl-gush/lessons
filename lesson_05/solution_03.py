84d2354380e0226b4a6f5889486e3272346734f5
Влад Гуштын <vladislav3372@gmail.com>
2022-05-31 22:35:15 +0300

    Fix classwork_01.py

--- lesson_05/solution_03.py
+++ lesson_05/solution_03.py
@@ -1,7 +1,9 @@
 """
-Написать функцию, которая используя модуль requests скачивает файл сохраняет его на файловой системе, функция имеет два
-параметра: ссылка на файл и имя на файловой системе. В качестве примера ссылки на файл можно использовать лицензию из
-ГитХаба из вашего репозитория: https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE
+Написать функцию, которая используя модуль requests скачивает файл сохраняет
+его на файловой системе, функция имеет два параметра: ссылка на файл и имя на
+файловой системе. В качестве примера ссылки на файл можно использовать лицензию
+из ГитХаба из вашего репозитория:
+https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE
 """
 import requests
 
@@ -12,4 +14,7 @@ def download_file(file_url, file_path):
         file.write(responce)
 
 
-download_file("https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE", "example")
+download_file(
+    "https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE",
+    "example"
+)

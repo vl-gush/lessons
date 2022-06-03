f67ed26c29380c4554acf4ba769960434cd7117c
Vladislav Gushtyn <vladislav3372@gmail.com>
2022-06-03 21:41:30 +0300

    Add classwork_01.py for lesson 7

---
+++ lesson_07/classwork_02.py
@@ -0,0 +1,25 @@
+"""
+Написать функцию, которая будет вызывать задержку выполнения программы случайным образом от 1-й до 5-ти секунд.
+Написать декоратор, который будет выводить на печать время выполнения этой функции (end_time - start_time).
+"""
+from datetime import datetime
+from time import sleep
+import random
+
+
+def my_decorator(func):
+    def execution_time():
+        start_time = datetime.now()
+        func()
+        end_time = datetime.now()
+        print("Execution time: ", end_time - start_time)
+    return execution_time
+
+
+@my_decorator
+def random_delay():
+    sleep(random.randint(1, 5))
+
+
+if __name__ == "__main__":
+    random_delay()
